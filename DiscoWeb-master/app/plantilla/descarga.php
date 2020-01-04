50b52679903654979bbcba4b4b35b49c067de77c
JaimeyJesus <58769676+JaimeyJesus@users.noreply.github.com>
2020-01-04 19:09:40 +0100

    Delete descarga.php

--- DiscoWeb-master/app/plantilla/descarga.php
+++
@@ -1,7 +0,0 @@
-<?php
-$enlace=$id."/".$archivo;
-header("Content-disposition: attachment; filename=$archivo");
-header("Content-type: application/octet-stream");
-header ("Content-Length: ".filesize($enlace));
-readfile($enlace);
-?>
